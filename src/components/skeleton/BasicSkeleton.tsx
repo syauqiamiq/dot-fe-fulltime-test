a5679474e4b40e7f136520878a678c0b09d67258
syauqiamiq <syauqiamiq12@gmail.com>
2024-07-22 20:32:28 +0700

    fix(env): remove env

---
+++ src/components/skeleton/BasicSkeleton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Skeleton } from "antd";
+import React from "react";
+
+interface IBasicSkeletonProps {
+	size?: "small" | "large" | "default" | undefined;
+	shape?: "default" | "circle" | "square" | "round" | undefined;
+}
+const BasicSkeleton = ({ size, shape }: IBasicSkeletonProps) => {
+	return (
+		<Skeleton.Button
+			className="dark:bg-gradient-to-r dark:from-gray-800  dark:to-slate-700"
+			active={true}
+			size={size}
+			shape={shape}
+			block={true}
+		/>
+	);
+};
+
+export default BasicSkeleton;
